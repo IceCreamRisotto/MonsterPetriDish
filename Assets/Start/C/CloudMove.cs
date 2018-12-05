6d9edf8342e8fb97ea0d65e03a0fce9e7514d52f
Naoloco <43401389+Naoloco@users.noreply.github.com>
2018-12-05 16:17:32 +0800

    Initial commit

---
+++ Assets/Start/C/CloudMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMove : MonoBehaviour {
+
+    private Transform me;
+    private Transform oldMe;
+
+	// Use this for initialization
+	void Start () {
+        me = GetComponent<Transform>();
+        StartCoroutine(Move());
+
+    }
+
+    IEnumerator Move()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            oldMe = me;
+            me.position = new Vector2(oldMe.position.x - 0.01f, oldMe.position.y);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Start/C/CloudTrans.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudTrans : MonoBehaviour {
+
+    public Transform[] CloudPosition;
+
+    public void CloudMoveing(string cloudID)
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("cloud"))
+        {
+            Transform oldVector = collision.transform;
+            collision.transform.position = new Vector2(5,oldVector.position.y);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
