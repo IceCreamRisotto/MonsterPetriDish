7a0039479d594953c141270d805596dcc7b2a7f0
Naoloco <43401389+Naoloco@users.noreply.github.com>
2018-12-17 13:41:05 +0800

    ui1

---
+++ Assets/Main/C/GameManager_Main.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_Main : MonoBehaviour {
+
+	public void OnMenu(GameObject ui)
+    {
+        ui.SetActive(false);
+    }
+}
