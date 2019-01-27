9227ed0c70ba25f473231cf7c3b1f036055f50d6
Naoloco <43401389+Naoloco@users.noreply.github.com>
2019-01-27 15:40:28 +0800

    金幣追加(beta)

---
+++ Assets/Main/C/GoldController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldController : MonoBehaviour {
+
+    GameObject Gold;
+
+	// Use this for initialization
+	void Start () {
+        //Gold = GetComponent<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void 
+}
