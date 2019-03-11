bc8704b06f1c6cf2835efa1f2204b9370b406cc0
ccc25687 <44232924+ccc25687@users.noreply.github.com>
2019-03-11 12:01:19 +0800

    infinite
    1

---
+++ Assets/explore/C/infinite.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class infinite : MonoBehaviour {
+    public float speed;
+    private Vector2 offset;
+    private float nowpos;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = new Vector2(0, 0);
+        GetComponent<Image>().material.SetTextureOffset("_MainTex", offset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nowpos += speed * Time.deltaTime;
+        offset = new Vector2(nowpos, 0);
+        GetComponent<Image>().material.SetTextureOffset("_MainTex", offset);
+    }
+}
