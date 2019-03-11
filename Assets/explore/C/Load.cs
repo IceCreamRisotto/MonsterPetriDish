33bf0ce68b0c3f91365ce768357a08ea69a7c042
ccc25687 <44232924+ccc25687@users.noreply.github.com>
2019-03-11 15:42:41 +0800

    ChangeSpace

---
+++ Assets/explore/C/Load.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Load : MonoBehaviour {
+
+    public SpriteRenderer[] background;
+    public int i;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            i = 0;
+            GetComponent<Image>().enabled = true;
+            GetComponent<Animation>().Play("Fade");
+        } else if (Input.GetKeyDown(KeyCode.W)){
+            i = 1;
+            GetComponent<Image>().enabled = true;
+            GetComponent<Animation>().Play("Fade");
+        }
+    }
+
+    void LoadMap( ){
+        GameObject.Find("背景卷軸").GetComponent<Image>().sprite=background[i].sprite;
+    }
+}
