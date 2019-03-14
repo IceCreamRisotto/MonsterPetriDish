2a04cd0803febe2d9ca6779142667ab1c9ff034d
Naoloco <43401389+Naoloco@users.noreply.github.com>
2019-03-14 15:59:56 +0800

    探索選單活性化

--- Assets/Main/C/GameManager_Main.cs
+++ Assets/Main/C/GameManager_Main.cs
@@ -15,8 +15,20 @@ public class GameManager_Main : MonoBehaviour {
     public GameObject[] explorationOnButton;
     public GameObject[] explorationOffButton;
 
-    public Image explorePicture;
-    public Button explorButton;
+    [Header("探索圖片")]
+    public Image explorePicture; //探索確認頁面的圖片
+
+    [Header("探索標題")]
+    public Text explorTitle; //探索確認頁面標題
+
+    [Header("探索副標")]
+    public Text explorTitleTwo; //探索確認頁面副標題(探索時間)
+
+    [Header("探索按鈕陣列")]
+    public Button[] explorButtons; //探索確認頁面的確認按鈕
+
+    [Header("探索確認頁文本")]
+    String[,] explorText=new string[2,2]; //探索確認頁面對應文本(第一維=第幾關&第二維=主副標題)
 
     Boolean explorationOnOff; //因為探索放在update，設置開關減少效能消耗
     DateTime explorationStartTime; //探索開始時間 備考:幾乎不需要
@@ -39,6 +51,13 @@ public class GameManager_Main : MonoBehaviour {
 
     public void Start()
     {
+
+        //探索確認選單，文本更新處
+        explorText[0,0] = "Level.1  滾筒木屋";
+        explorText[0,1] = "探索時間 半 小時";
+        explorText[1,0] = "Level.2  洞窟";
+        explorText[1,1] = "探索時間 1 小時";
+
         //a= GetTime(GetTimeStamp());
         //b= (01:00:00);
         if (PlayerPrefs.HasKey("explorationNo"))
@@ -58,7 +77,8 @@ public class GameManager_Main : MonoBehaviour {
             explorationNo = 0;
             explorationOnOff = false;
         }
-        
+
+
     }
 
     private void Update()
@@ -108,7 +128,13 @@ public class GameManager_Main : MonoBehaviour {
         explorationStartTime = GetTime(GetTimeStamp());
         //PlayerPrefs.SetString("explorationStartTime", explorationStartTime.ToString());
         //判斷哪一個探索關卡，給予endTime相應的加長時間
-        if(explorationNumber==2)//洞窟
+        if (explorationNumber == 1)//滾筒木屋
+        {
+            //explorationEnd = new DateTime(nullTime.Year, nullTime.Month, nullTime.Day, nullTime.Hour+1, nullTime.Minute, nullTime.Second);
+            explorationEndTime = new DateTime(explorationStartTime.Year, explorationStartTime.Month, explorationStartTime.Day, explorationStartTime.Hour, explorationStartTime.Minute+30, explorationStartTime.Second);
+            PlayerPrefs.SetString("explorationEndTime", explorationEndTime.ToString());
+        }
+        if (explorationNumber==2)//洞窟
         {
             //explorationEnd = new DateTime(nullTime.Year, nullTime.Month, nullTime.Day, nullTime.Hour+1, nullTime.Minute, nullTime.Second);
             explorationEndTime = new DateTime(explorationStartTime.Year, explorationStartTime.Month, explorationStartTime.Day, explorationStartTime.Hour + 1, explorationStartTime.Minute, explorationStartTime.Second);
@@ -156,6 +182,10 @@ public class GameManager_Main : MonoBehaviour {
 
     public void exploreButton(int exploreNumber) //點擊探索關卡，修改任務介面
     {
-        explorePicture.sprite = Resources.Load("explore/"+exploreNumber, typeof(Sprite)) as Sprite;
+        explorTitle.text = explorText[exploreNumber-1,0]; //修改標題
+        explorTitleTwo.text = explorText[exploreNumber - 1,1]; //修改花費時間
+        explorePicture.sprite = Resources.Load("explore/"+exploreNumber, typeof(Sprite)) as Sprite; //改圖
+        Block talkBlock = gamemanagerFlowchart.FindBlock("探索選單出現"); //呼叫確認頁面出現
+        gamemanagerFlowchart.ExecuteBlock(talkBlock);
     }
 }
