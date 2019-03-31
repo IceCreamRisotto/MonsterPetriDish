1b53c5e96a912418c125a2c7404c359c36c12239
ccc25687 <44232924+ccc25687@users.noreply.github.com>
2019-04-01 00:42:14 +0800

    level_button

---
+++ Assets/Level/C/Click_event.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Click_event : MonoBehaviour {
+
+     public GameObject image;
+     public bool click_type = false;
+
+    private void Update()
+    {
+        if (click_type) {
+            image.GetComponent<Image>().color = new Color32(200, 200, 200, 255);
+        }
+    }
+
+    public void press(bool start) {
+         click_type = start;
+         if (click_type)
+         {
+            image.GetComponent<Image>().color = new Color32(200,200,200,255);
+        }
+         else if(!click_type){
+            image.GetComponent<Image>().color = new Color32(255,255,255,200);
+        }
+     }
+}
