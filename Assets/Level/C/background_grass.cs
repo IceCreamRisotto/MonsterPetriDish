a291eb557bfe89c5f486e599b4855d19ae86b048
ccc25687 <44232924+ccc25687@users.noreply.github.com>
2019-03-31 17:19:28 +0800

    level1_background

---
+++ Assets/Level/C/background_grass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_grass : MonoBehaviour {
+
+    public Transform[] background;
+    public float background_speed;
+    public Transform limit_Left;
+    public Transform limit_Right;
+    
+    void Update()
+    {
+        foreach (Transform i in background)
+        {
+            i.position = new Vector3(i.position.x - background_speed * Time.deltaTime, i.position.y, i.position.z);
+            if (i.position.x <= limit_Left.position.x)
+            {
+                i.position = new Vector3(limit_Right.position.x, i.position.y, i.position.z);
+            }
+        }
+    }
+}
