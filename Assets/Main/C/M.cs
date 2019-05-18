ea598be102f31db3dcb5e6e59c84eab7ab271d34
Naoloco <43401389+Naoloco@users.noreply.github.com>
2019-05-18 17:46:06 +0800

    Revert "主畫面腳色點擊(修復)"
    This reverts commit fe1c654e37d31f75a185e2df8ae6d90236904f6d.

--- Assets/Main/C/M.cs
+++ Assets/Main/C/M.cs
@@ -24,11 +24,10 @@ public class M : MonoBehaviour {
     private int states;
 
     public Transform turn;
-    public float v3 = 0.5f;
+    public float v3=0.5f;
 
     public Transform lim_UpperRight;
     public Transform lim_BottomLeft;
-
 
     private void Awake()
     {
@@ -39,7 +38,7 @@ public class M : MonoBehaviour {
 
     private void Update()
     {
-        click_off();//判斷點擊後跳回動作
+
 
         if (coRou == null) {//等太久會睡覺
             coRou = StartCoroutine(sleep());
@@ -139,23 +138,16 @@ public class M : MonoBehaviour {
     }
 
     public void click_off() {
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("click")||
-            animator.GetCurrentAnimatorStateInfo(0).IsName("click_magic")||
-            animator.GetCurrentAnimatorStateInfo(0).IsName("click_mushroom")) {
-            AnimatorStateInfo info = animator.GetCurrentAnimatorStateInfo(0);
-            if (info.normalizedTime >= 1.0f)
-            {
-                coRou = null;
-                coRou2 = null;
-                states = 1;
-                animator.SetInteger("states", states);
-                //Debug.Log("點集中");
-            }
-        }
+        coRou = null;
+        coRou2 = null;
+        states = 1;
+        animator.SetInteger("states", states);
     }
 
 
     public void mega() {
+
+
 
         if (chickenID == 0)
         {
