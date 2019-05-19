0055ec739075e5f98d440bca5b175a3430fae6a7
ccc25687 <ccc25687@gmail.com>
2019-05-19 13:36:39 +0800

    家事件背景重製

---
+++ Assets/explore/C/FloorMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloorMove : MonoBehaviour {
+    public float speed;
+    private Transform left_lmt, right_lmt;
+
+    private void Awake()
+    {
+        left_lmt= GameObject.Find("背景移動").GetComponent<parameter>().left_lmt;
+        right_lmt = GameObject.Find("背景移動").GetComponent<parameter>().right_lmt;
+    }
+    private void Update()
+    {
+        re_speed();
+        move();
+        lmt();
+    }
+
+    private void re_speed() {
+        speed = GameObject.Find("背景移動").GetComponent<parameter>().floor_speed;
+    }
+
+    void move() {
+        transform.position = new Vector3(transform.position.x-speed, transform.position.y, transform.position.z);
+    }
+
+    void lmt() {
+        if (transform.position.x <= left_lmt.transform.position.x) {
+            transform.position = new Vector3(right_lmt.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/explore/C/parameter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class parameter : MonoBehaviour {
+    public float floor_speed;
+    public Transform left_lmt, right_lmt;
+
+    public Animator player;
+
+    private void Update()
+    {
+        player.speed = floor_speed /1.1f;
+    }
+
+}
