6a34a28e82cf90b1e082715c5c0aec8b16067825
Naoloco <43401389+Naoloco@users.noreply.github.com>
2019-05-21 17:36:00 +0800

    = = = =

---
+++ Assets/Main/C/DeBug/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour {
+
+    GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = FindObjectOfType<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TestItemGet()
+    {
+        for(int i=0;i>gameManager.items.Length;i++)
+        {
+            gameManager.items[i] += 10;
+        }
+    }
+
+}
