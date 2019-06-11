e9f98e3066f4b57c639d13885a1bd617f07ebfd3
Naoloco <43401389+Naoloco@users.noreply.github.com>
2019-06-11 11:19:52 +0800

    .

---
+++ Assets/Resources/Script/NewPlayerTeacher.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPlayerTeacher : MonoBehaviour {
+
+    //存檔物件名稱(依據場景修改)
+    [Header("存檔物件名稱")]
+    public string playerPrefsName;
+
+    //場景序計數器
+    [Header("場景序")]
+    public int sceneCount;
+
+    //場景序計數器終點(依據場景修改)
+    [Header("場景序終點")]
+    public int sceneCountEnd;
+
+    [Header("當前場景文字")]
+    public string[] sceneTalk;
+
+    
+
+	// Use this for initialization
+	void Start () {
+        //先判斷該場景是否已經做完新手教學
+        if(PlayerPrefs.HasKey(playerPrefsName))
+        {
+            //場景序=上次中斷時場景 || 新場景0
+            sceneCount = PlayerPrefs.GetInt(playerPrefsName);
+        }
+        else
+        {
+            sceneCount = 0;
+            PlayerPrefs.SetInt(playerPrefsName, 0);
+        }
+
+        if(sceneCount != sceneCountEnd)
+        {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
