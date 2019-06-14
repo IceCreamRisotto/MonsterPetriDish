3eccce44975865b1ab8bfbac0383d6014efad8be
ccc25687 <ccc25687@gmail.com>
2019-06-14 13:02:30 +0800

    教學

---
+++ Assets/explore/C/NewGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class NewGame : MonoBehaviour {
+    Flowchart flowchart;
+    public int scene {
+        get { return flowchart.GetIntegerVariable("SceneCount"); }
+        set { flowchart.SetIntegerVariable("SceneCount",value); }
+    }
+    private void Start()
+    {
+        flowchart = FindObjectOfType<Flowchart>();
+    }
+    public void count() {
+        
+    }
+}
